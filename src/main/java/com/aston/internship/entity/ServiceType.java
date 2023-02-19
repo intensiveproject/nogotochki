47ac9feb26ebd13ef05b53398bf3fbd1cfae2c17
j-thrash <j-thrash@ya.ru>
2023-02-19 13:58:32 +0300

    Created base structure
    Fix mistakes
    
    Create new structure
    
    Create new structure
    
    Fix new structure
    
    Fix new structure
    
    Fix new structure

---
+++ src/main/java/com/aston/internship/entity/ServiceType.java
@@ -0,0 +1,29 @@
+package com.aston.internship.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "SERVICE_TYPE")
+@AllArgsConstructor
+@Data
+
+public class ServiceType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @OneToOne
+    private Notation notation;
+
+    public ServiceType() {
+    }
+}

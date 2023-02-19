0407ff23568e45a0f8c3d93c97ff5c15564b442b
Busygin Mikhail <izerg556@gmail.com>
2023-02-19 11:24:10 +0400

    create structure and producer table

---
+++ src/main/java/com/aston/internship/Nogotochki.java
@@ -0,0 +1,7 @@
+package com.aston.internship;
+
+public class Nogotochki {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/aston/internship/NogotochkiTest.java
@@ -0,0 +1,12 @@
+package com.aston.internship;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class NogotochkiTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}

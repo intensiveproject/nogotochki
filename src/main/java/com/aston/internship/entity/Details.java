2ed3c76384ae8ef32202d5b381d848fcdcdf2c09
Busygin Mikhail <izerg556@gmail.com>
2023-03-01 09:59:49 +0400

    security config

--- src/main/java/com/aston/internship/entity/Details.java
+++ src/main/java/com/aston/internship/entity/Details.java
@@ -6,15 +6,24 @@ import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Data
 @Embeddable
-public class Details {
+public class Details implements UserDetails {
 
     private String name;
     private String surname;
 
+    @Size(min=5, message = "Не меньше 5 знаков")
+    private String username;
     @Size(min=5, message = "Не меньше 5 знаков")
     private String password;
     @Transient
@@ -23,6 +32,43 @@ public class Details {
     private String phone;
     private String email;
 
+    private Set<String> roles;
+
     public Details() {
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
---
+++ src/main/java/com/aston/internship/service/CommonUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.aston.internship.service;
+
+import com.aston.internship.repository.DetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CommonUserDetailsService implements UserDetailsService {
+    private final DetailsRepository detailsRepository;
+
+    @Autowired
+    public CommonUserDetailsService(DetailsRepository detailsRepository) {
+        this.detailsRepository = detailsRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        return detailsRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
+    }
+}
